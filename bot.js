e9b86f5501d2f78c119b64aa7edde4ae3990b8a1
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-09-22 15:40:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -123,6 +123,15 @@ client.on('message', function(msg) {
   });
 //newcode
 
+client.on('message', msg => {
+  if(msg.content === '*inv')
+              setTimeout(function() {  
+msg.reply('http://cutt.us/P8eIJ')
+              }, 10000)
+});
+
+//newcode
+
 const adminprefix = "*";
 const devs = ['427054141492297728','ايدي اخويك'];
 client.on('message', message => {
@@ -410,6 +419,70 @@ client.on("message", function(message) {
 
 }); 
 
+//newchat
+
+client.on('message', msg => {
+  if(msg.content === '*support')
+              setTimeout(function() {  
+msg.reply('https://discord.gg/Mwyp8d8')
+              }, 10000)
+});
+
+//newcode
+
+client.on('message', message => {
+  var prefix = "*"
+var args = message.content.split(" ").slice(1);    
+if(message.content.startsWith(prefix + 'id')) {
+var year = message.author.createdAt.getFullYear()
+var month = message.author.createdAt.getMonth()
+var day = message.author.createdAt.getDate()
+var men = message.mentions.users.first();  
+let args = message.content.split(' ').slice(1).join(' ');
+if (args == '') {
+var z = message.author;
+}else {
+var z = message.mentions.users.first();
+}
+
+let d = z.createdAt;          
+let n = d.toLocaleString();   
+let x;                       
+let y;                        
+
+if (z.presence.game !== null) {
+y = `${z.presence.game.name}`;
+} else {
+y = "No Playing... |💤.";
+}
+if (z.bot) {
+var w = 'بوت';
+}else {
+var w = 'عضو';
+}
+let embed = new Discord.RichEmbed()
+.setColor("#502faf")
+.addField('🔱| اسمك:',`**<@` + `${z.id}` + `>**`, true)
+.addField('🛡| ايدي:', "**"+ `${z.id}` +"**",true)
+.addField('♨| Playing:','**'+y+'**' , true)
+.addField('🤖| نوع حسابك:',"**"+ w + "**",true)    
+.addField('📛| الكود حق حسابك:',"**#" +  `${z.discriminator}**`,true)
+.addField('**التاريح الذي انشئ فيه حسابك | 📆 **: ' ,year + "-"+ month +"-"+ day)    
+.addField("**تاريخ دخولك للسيرفر| ⌚   :**", message.member.joinedAt.toLocaleString())    
+
+.addField('**⌚ | تاريخ انشاء حسابك الكامل:**', message.author.createdAt.toLocaleString())
+.addField("**اخر رسالة لك | 💬  :**", message.author.lastMessage)            
+
+.setThumbnail(`${z.avatarURL}`)
+.setFooter(message.author.username, message.author.avatarURL)
+
+message.channel.send({embed});
+  if (!message) return message.reply('**ضع المينشان بشكل صحيح  ❌ **').catch(console.error);
+
+}
+
+});
+
 //newcode
 
 client.on('message', message => {
@@ -423,6 +496,8 @@ client.on('message', message => {
 .addField('     ** *user **  ' ,' **لعمل روم يكتب كم عضو ب السيرفر** ')
 .addField('     ** *mute **  ' ,' **لعمل ميوت كتابي لحد** ')
 .addField('     ** *unmute ** ' ,' **لفك الميوت الكتابي** ')
+.addField('     ** *cchat **  ' ,' **لتقفيل الشات ما احد يقدر يكتب به** ')
+.addField('     ** *uchat **  ' ,' **لفتح الشات تاني** ')
 .addField('     ** **  ' ,' ** ** ')
 .addField('**لدعوة البوت لسيرفرك ..**' , '**http://cutt.us/P8eIJ**')
 .setColor('RANDOM')
