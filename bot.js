7dec449e82b97d9be7370b8410e64e4fbc2a88f4
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-09-22 15:57:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -171,8 +171,6 @@ client.on('message', function(msg) {
 //newcode
 
 //    Packages
-const Discord = require('discord.js');
-const client = new Discord.Client();
 const fs = require("fs"); 
 const ytdl = require("ytdl-core");
 const { Client, Util } = require('discord.js');
