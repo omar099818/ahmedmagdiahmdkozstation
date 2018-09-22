f44f64a2089740ba82a03a542e51770bb214ec27
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-09-22 17:30:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ client.on('ready', () => {
 
 //newcode
 
-var prefix = "*";
+var prefix = '*';
 client.on('message', message => {
 if(message.author.bot) return;
    if(!message.channel.guild) return;
@@ -37,7 +37,7 @@ if(!message.channel.guild) return message.channel.send('**This Command is Just F
 if(!message.member.hasPermission('MANAGE_MESSAGES')) return      message.channel.send('**You Do not have permission** `MANAGE_MESSAGES`' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
 let request = `Requested By ${message.author.username}`;
-message.channel.send(`**Are You sure you want to clear the chat?**`).then(msg => {
+message.channel.send(`**هل أنت متأكد من أنك تريد مسح الشات؟**`).then(msg => {
 msg.react('✅')
 .then(() => msg.react('❌'))
 .then(() =>msg.react('✅'))
@@ -48,13 +48,13 @@ let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.
 let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
 let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
 reaction1.on("collect", r => {
-message.channel.send(`Chat will delete`).then(m => m.delete(5000));
+message.channel.send(`جاري حذف الشات`).then(m => m.delete(5000));
 var msg;
         msg = parseInt();
 
       message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
       message.channel.sendMessage("", {embed: {
-        title: "`` Chat Deleted ``",
+        title: "`` تم مسح الشات ``",
         color: 0x06DF00,
         footer: {
 
