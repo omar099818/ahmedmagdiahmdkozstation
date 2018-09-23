2c4d0511ce1a89389f0e45234f7d60f48bf70bda
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-09-23 16:22:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,6 +28,27 @@ client.on('ready', () => {
 
 //newcode
 
+///ping
+client.on('message', message => {
+    if (message.author.bot) return
+                                if(!message.channel.guild) return;
+                        if (message.content.startsWith('*ping')) {
+                            if(!message.channel.guild) return;
+                            var msg = `${Date.now() - message.createdTimestamp}`
+                            var api = `${Math.round(client.ping)}`
+                            if (message.author.bot) return;
+                        let embed = new Discord.RichEmbed()
+                        .setAuthor(message.author.username,message.author.avatarURL)
+                        .setColor('RANDOM')
+                        .addField('**Time Taken:**',msg + " ms :signal_strength: ")
+                        .setFooter(`namebot `,client.user.avatarURL)
+         message.channel.send({embed:embed});
+                        }
+                    });
+ 
+
+//newcode
+
 client.on('message', message => {
 if(message.content == '*bot') {
          if(!message.author.id === '427054141492297728') return;
