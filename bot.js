26cf921d4ad6eac13f17f4e0c699fa31a0fce091
AhmedMagdyi <43391576+AhmedMagdyi@users.noreply.github.com>
2018-09-23 18:04:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,6 +28,49 @@ client.on('ready', () => {
 
 //newcode
 
+client.on('message', message => {
+if(message.author.bot) return;
+   if(!message.channel.guild) return;
+if(message.content.startsWith(prefix + 'clear')) {
+if(!message.channel.guild) return message.channel.send('**This Command is Just For Servers**').then(m => m.delete(5000));
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return      message.channel.send('**You Do not have permission** `MANAGE_MESSAGES`' );
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+let request = `Requested By ${message.author.username}`;
+message.channel.send(`**هل أنت متأكد من أنك تريد مسح الشات؟**`).then(msg => {
+msg.react('✅')
+.then(() => msg.react('❌'))
+.then(() =>msg.react('✅'))
+
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+reaction1.on("collect", r => {
+message.channel.send(`جاري حذف الشات`).then(m => m.delete(5000));
+var msg;
+        msg = parseInt();
+
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "`` تم مسح الشات ``",
+        color: 0x06DF00,
+        footer: {
+
+        }
+      }}).then(msg => {msg.delete(3000)});
+
+})
+reaction2.on("collect", r => {
+message.channel.send(`**Chat deletion cancelled**`).then(m => m.delete(5000));
+msg.delete();
+})
+})
+}
+});
+
+//newcode
+
 //    Packages
 const fs = require("fs"); 
 const ytdl = require("ytdl-core");
@@ -83,7 +126,7 @@ if (message.content === 'fhelp') {
 
   client.on('message', message => {
    if (message.author.bot) return;
-    if (message.content === "*help") {
+    if (message.content === "*mhelp") {
               if(!message.channel.guild) return message.reply(':x:  **The orders are not in your الاوامر مو فى **');
  const embed = new Discord.RichEmbed()
          .setColor("#efa800")
@@ -942,6 +985,8 @@ client.on('message', message => {
 .setThumbnail(message.author.avatarURL)
 .addField('     **اوامر عامه** ' ,' **الاوامر العامه الخاصه ب الزاور** ')
 .addField('     ** **  ' ,' ** ** ')
+.addField('     ** *clear **  ' ,' **لمسح جميع الرسايل ال ب الروم** ')
+.addField('     ** *mhelp **  ' ,' **لعرض اوامر كود الاغاني** ')
 .addField('     ** *support **  ' ,' **لسيرفر الدعم الفني** ')
 .addField('     ** *inv **  ' ,' **لدعوة البوت لسيرفرك** ')
 .addField('     ** *id **  ' ,' **لمعرفة معلوماتك الخاصه** ')
